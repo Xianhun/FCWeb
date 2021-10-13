f45533f0fbcb67dca887d07382ef4c597e6da399
特训班20-黄意镐 <@CHTHOLLY>
2021-10-13 23:21:17 +0800

    添加项目文件。

---
+++ FCWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FCWeb.MvcApplication" Language="C#" %>
---
+++ FCWeb/Models/FCDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace FCWeb.Models
+{
+    public class FCDbContext : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“FCDbContext”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“FCWeb.Models.FCDbContext”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“FCDbContext”
+        //连接字符串。
+        public FCDbContext()
+            : base("name=FCDbContext")
+        {
+        }
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
